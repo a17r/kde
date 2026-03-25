a17r/kde

commit ba1f14928159fd2f555a5cfce7a2e7a937ad5018

kde-apps/kdenetwork-filesharing: refresh dependencies
Upstream commits:
* 7a5f26b9b4528f25d4ccd62867cc12a7d77ae170
* 198d130e6cf69283714d04ae1f4e844ea74f5995

Signed-off-by: Sam James <sam@gentoo.org>

kde-apps/kdenetwork-filesharing/kdenetwork-filesharing-9999.ebuild
@@ -13,10 +13,10 @@ HOMEPAGE="https://apps.kde.org/kdenetwork_filesharing/"
 LICENSE="GPL-2" # TODO: CHECK
 SLOT="6"
 KEYWORDS=""
-IUSE=""
+IUSE="systemd"
 
 DEPEND="
-	dev-libs/qcoro
+	dev-libs/qcoro[dbus.qml]
 	>=dev-qt/qtbase-${QTMIN}:6[dbus,gui,network,widgets]
 	>=dev-qt/qtdeclarative-${QTMIN}:6
 	>=kde-frameworks/kauth-${KFMIN}:6
@@ -34,6 +34,7 @@ RDEPEND="${DEPEND}
 src_configure() {
 	local mycmakeargs=(
 		-DCMAKE_DISABLE_FIND_PACKAGE_PackageKitQt6=ON
+		-DUSE_SYSTEMD=$(usex systemd)
 	)
 
 	ecm_src_configure

commit 23b169cf54128b4071080ca2b4a954e5359363a2

kde-apps/kdenetwork-filesharing: fix typo
Fixes: ba1f14928159fd2f555a5cfce7a2e7a937ad5018
Signed-off-by: Sam James <sam@gentoo.org>

kde-apps/kdenetwork-filesharing/kdenetwork-filesharing-9999.ebuild
@@ -16,7 +16,7 @@ KEYWORDS=""
 IUSE="systemd"
 
 DEPEND="
-	dev-libs/qcoro[dbus.qml]
+	dev-libs/qcoro[dbus,qml]
 	>=dev-qt/qtbase-${QTMIN}:6[dbus,gui,network,widgets]
 	>=dev-qt/qtdeclarative-${QTMIN}:6
 	>=kde-frameworks/kauth-${KFMIN}:6